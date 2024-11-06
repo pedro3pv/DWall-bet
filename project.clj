7ef500fd538858c72a73a4ac4c4bd984cfa74aa5
NetoDantas270 <132476169+NetoDantas270@users.noreply.github.com>
2024-11-06 16:41:28 -0300

    add dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"] [compojure "1.7.1"] [com.novemberain/monger "3.5.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [io.pedestal/pedestal.service "0.5.7"]
+                 [io.pedestal/pedestal.route "0.5.7"]
+                 [io.pedestal/pedestal.jetty "0.5.7"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.slf4j/slf4j-simple "1.7.28"]
+                 [com.novemberain/monger "3.5.0"]
+                 [cheshire "5.13.0"]]
   :repl-options {:init-ns app.core})
