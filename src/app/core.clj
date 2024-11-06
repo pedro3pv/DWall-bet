acd4a29bb6caeefa89b5d714c71ac8dcb8ecc965
NetoDantas270 <132476169+NetoDantas270@users.noreply.github.com>
2024-11-06 17:43:56 -0300

    added middleware

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,6 +1,27 @@
-(ns app.core)
+(ns app.core
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [app.middleware :refer [json-interceptor]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn funcao-hello [request]
+  {:status 200
+   :body "Hello World"})
+
+(defn json-handler [request]
+  (println "Dados recebidos:" (:json-body request))
+  {:status 200
+   :body {:mensagem "Dados recebidos com sucesso!"}})
+
+(def routes (route/expand-routes
+              #{["/hello" :get funcao-hello :route-name :hello-world]
+                ["/Json" :post [json-interceptor json-handler] :route-name :Json]}))
+
+(def http-server {
+                  ::http/routes routes
+                  ::http/port 8080
+                  ::http/type :jetty
+                  ::http/join? false
+                  })
+
+(http/start (http/create-server http-server))
+(println "Server started on port 8080")
