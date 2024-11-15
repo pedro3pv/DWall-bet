e26bf015f15d30e5fc40a51514464c28e06b985e
pedro3pv <53156040+pedro3pv@users.noreply.github.com>
2024-11-15 16:20:54 -0300

    add inicial method for process data

--- src/app/aposta.clj
+++ src/app/aposta.clj
@@ -1,6 +1,6 @@
 (ns app.aposta
   (:require [app.db.db-saldo :refer [get-saldo remove-saldo]]
-            [app.db.db-apostas :refer [add-aposta db-apostas]]
+            [app.db.db-apostas :refer [add-aposta db-apostas remove-aposta]]
             [app.service.rundown :refer [fetch-event-details]]))
 
 (defn criar-aposta [request]
@@ -31,7 +31,32 @@
    :body {:mensagem "Lista de apostas criadas."
           :apostas @db-apostas}})
 
+(defn calculate-odd [aposta]
+  (let [event-odds (get-in aposta [:lines :moneyline])]
+    (cond
+      (and (< (:moneyline_home event-odds) 0) (= (int (:mercado event-odds)) 0)) (Math/abs (/ 100 (:moneyline_home event-odds)))
+      (and (> (:moneyline_home event-odds) 0) (= (int (:mercado event-odds)) 1)) (inc (/ (:moneyline_home event-odds) 100))
+      (and (< (:moneyline_away event-odds) 0) (= (int (:mercado event-odds)) 2)) (Math/abs (/ 100 (:moneyline_away event-odds)))
+      (and (> (:moneyline_away event-odds) 0) (= (int (:mercado event-odds)) 3)) (inc (/ (:moneyline_away event-odds) 100))
+      :else 1)))
 
 (defn update-bets [request]
-
-)
+  (try
+    (let [apostas @db-apostas
+          filter_bets (filter #(and (= (:status %) "ativo") 
+                                    (<= (:event_date %) (java.time.Instant/now))) apostas)
+          event_details (map #(fetch-event-details (:event-id %)) filter_bets)
+          filter_event_details (filter #(= (get-in % [:score :event_status]) "STATUS_FINAL") event_details)
+          event-ids (set (map :id filter_event_details))
+          filtered-apostas (filter #(contains? event-ids (:id %)) apostas)
+          final_bets (map #(assoc % :status "finalizado") filtered-apostas)]
+      (dorun (map #(remove-aposta (:id %)) filtered-apostas))
+      (dorun (map #(add-saldo (calculate-odd %)) filtered-apostas))
+      (dorun (map #(add-aposta %) final_bets))
+      {:status 200
+       :body {:mensagem "Apostas atualizadas com sucesso!"
+              :apostas final_bets}})
+    (catch Exception e
+      {:status 500
+       :body {:mensagem "Ocorreu um erro ao atualizar as apostas."
+              :erro (.getMessage e)}}))
