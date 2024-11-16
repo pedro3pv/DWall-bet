9cece2a426266a2f2d4113620e7ef31323705ad5
pedro3pv <53156040+pedro3pv@users.noreply.github.com>
2024-11-16 14:19:20 -0300

    fix double convertion error

--- src/app/aposta.clj
+++ src/app/aposta.clj
@@ -50,15 +50,19 @@
     (< value 0) (inc (double (abs (/ 100 value))))
     (> value 0) (double (/ value 100))))))
 
-(defn calculate-handicap-home [pontos_home pontos_away handicap-casa handicap-fora odds-casa odds-fora]
+(defn calculate-handicap-home [pontos_home pontos_away handicap-casa odds-casa]
+(if (or (= handicap-casa 1.0E-4) (= odds-casa 1.0E-4))
+  0
   (cond
-    (and (> pontos_home pontos_away) (> (+ pontos_home handicap-casa) pontos_away)) odds-casa
-    :else 0))
+    (and (> pontos_home pontos_away) (> (+ pontos_home handicap-casa) pontos_away)) (double odds-casa)
+    :else 0)))
   
-(defn calculate-handicap-away [pontos_home pontos_away handicap-casa handicap-fora odds-casa odds-fora]
+(defn calculate-handicap-away [pontos_home pontos_away handicap-fora odds-fora]
+(if (or (= handicap-fora 1.0E-4) (= odds-fora 1.0E-4))
+  0
   (cond
-    (and (> pontos_away pontos_home) (> (+ pontos_away handicap-fora) pontos_home)) odds-fora
-    :else 0))
+    (and (> pontos_away pontos_home) (> (+ pontos_away handicap-fora) pontos_home)) (double odds-fora)
+    :else 0)))
     
 
 (defn calculate-odd [aposta api_result]
@@ -68,15 +72,15 @@
   (def resultado_away (int (get-in api_result [:score :winner_away])))
   (def pontos_home (int (get-in api_result [:score :score_home])))
   (def pontos_away (int (get-in api_result [:score :score_away])))
-  (def handicap-casa (double (get-in aposta [:event-details :lines :spread :point_spread_home])))
-  (def handicap-fora (double (get-in aposta [:event-details :lines :spread :point_spread_away])))
-  (def odds-casa (double (get-in aposta [:event-details :lines :spread :point_spread_home_money])))
-  (def odds-fora (double (get-in aposta [:event-details :lines :spread :point_spread_away_money])))
+  (def handicap-casa (get-in aposta [:event-details :lines :spread :point_spread_home]))
+  (def handicap-fora (get-in aposta [:event-details :lines :spread :point_spread_away]))
+  (def odds-casa (get-in aposta [:event-details :lines :spread :point_spread_home_money]))
+  (def odds-fora (get-in aposta [:event-details :lines :spread :point_spread_away_money]))
   (cond
     (and (= mercado 0) (= resultado_away 1)) (calculate-moneyline-away list_lines)
     (and (= mercado 1) (= resultado_home 1)) (calculate-moneyline-home list_lines)
-    (and (= mercado 2) (= resultado_home 1)) (calculate-handicap-home pontos_home pontos_away handicap-casa handicap-fora odds-casa odds-fora)
-    (and (= mercado 3) (= resultado_away 1)) (calculate-handicap-away pontos_home pontos_away handicap-casa handicap-fora odds-casa odds-fora)
+    (and (= mercado 2) (= resultado_home 1)) (calculate-handicap-home pontos_home pontos_away handicap-casa odds-casa)
+    (and (= mercado 3) (= resultado_away 1)) (calculate-handicap-away pontos_home pontos_away handicap-fora odds-fora)
     :else 0))
 
 (defn update-bets [request]
@@ -91,9 +95,9 @@
       (def filter_event_details (filter #(= (get-in % [:score :event_status]) "STATUS_FINAL") event_details))
       (def event-ids (set (map #(get-in % [:event_id]) filter_event_details)))
       (def filtered-apostas (filter #(contains? event-ids (get-in % [:event-details :event_id])) @db-apostas))
-      (def final_bets (map #(assoc % :status "finalizado") filtered-apostas))
       (dorun (map #(remove-aposta (get-in % [:id])) filtered-apostas))
       (dorun (map #(add-saldo (* (double (:valor %)) (calculate-odd %1 %2))) filtered-apostas filter_event_details))
+      (def final_bets (map #(if (= (calculate-odd %1 %2) 0) (assoc % :status "perdido") (assoc % :status "vencido")) filtered-apostas filter_event_details))
       (dorun (map #(add-aposta %) final_bets))
       {:status 200
        :body {:mensagem "Apostas atualizadas com sucesso!"
