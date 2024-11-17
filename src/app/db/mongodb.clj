00103b02ac4ac42340129b5a1545dd4845050d9f
pedro3pv <53156040+pedro3pv@users.noreply.github.com>
2024-11-17 15:28:33 -0300

    update for new driver (library)

--- src/app/db/mongodb.clj
+++ src/app/db/mongodb.clj
@@ -1,57 +1,48 @@
 (ns app.db.mongodb
-(:require [monger.core :as mg]
-            [monger.collection :as mc])
-  (:import [org.bson.types ObjectId]
-           [com.mongodb DB WriteConcern]))
+  (:require [mongo-driver-3.client :as mcl]
+            [mongo-driver-3.collection :as mc]
+            [mongo-driver-3.operator :refer [$eq $set]]))
 
 (defn start-connection []
-  (def uri "mongodb+srv://pedro3pv:#&Hx2qsapVxwN)V1@dwall.rmj4r.mongodb.net/?retryWrites=true&w=majority&appName=DWall")
-  (def {:keys [conn db]} (mg/connect-via-uri uri))
-  [conn db]
-)
+  (let [uri "mongodb+srv://pedro3pv:wytgSg993QWwsics@dwall.rmj4r.mongodb.net/DWall?retryWrites=true&w=majority&appName=DWall"]
+    (mcl/connect-to-db uri)))
 
-(defn disconnect [conn]
-  (mg/disconnect conn)
-)
+(defn disconnect [client]
+  (.close client))
 
 (defn add-aposta-mongodb [aposta]
-(def db_array (start-connection))
-(def db (second db_array))
-(mc/insert db "apostas" { :_id (:id aposta) :aposta aposta})
-(disconnect (first db_array))
-)
+  (let [db (start-connection)
+        client (:client db)
+        db-name (:db db)]
+    (mc/insert-one db-name "apostas" {:_id (:id aposta) :aposta aposta})
+    (disconnect client)))
 
 (defn get-apostas-mongodb []
-(def db_array (start-connection))
-(def db (second db_array))
-(mc/find db "apostas" {})
-(disconnect (first db_array))
-)
+  (let [db (start-connection)
+        client (:client db)
+        db-name (:db db)]
+    (mc/find db-name "apostas" {})
+    (disconnect client)))
 
 (defn remove-aposta-mongodb [id]
-(def db_array (start-connection))
-(def db (second db_array))
-(mc/remove db "apostas" {:_id id})
-(disconnect (first db_array))
-)
+  (let [db (start-connection)
+        client (:client db)
+        db-name (:db db)]
+    (mc/delete-one db-name "apostas" {:_id id})
+    (disconnect client)))
 
 (defn add-saldo-mongodb [saldo]
-(def db_array (start-connection))
-(def db (second db_array))
-(mc/insert db "saldos" { :_id 0 :saldo saldo})
-(disconnect (first db_array))
-)
+  (let [db (start-connection)
+        client (:client db)
+        db-name (:db db)]
+    (if (empty? (mc/find db-name "saldos" {}))
+      (mc/insert-one db-name "saldos" {:_id 0 :saldo saldo})
+      (mc/update-one db-name "saldos" {:_id 0} {$set {:saldo saldo}}))
+    (disconnect client)))
 
 (defn get-saldo-mongodb []
-(def db_array (start-connection))
-(def db (second db_array))
-(mc/find db "saldos" {})
-(disconnect (first db_array))
-)
-
-(defn update-saldo-mongodb [saldo]
-(def db_array (start-connection))
-(def db (second db_array))
-(mc/update db "saldos" {:_id 0} { :$set { :saldo saldo}})
-(disconnect (first db_array))
-)
+  (let [db (start-connection)
+        client (:client db)
+        db-name (:db db)]
+    (mc/find db-name "saldos" {})
+    (disconnect client)))
