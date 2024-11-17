73de3fe8337c35c02077f5a462a19d2a7d5db889
pedro3pv <53156040+pedro3pv@users.noreply.github.com>
2024-11-17 15:29:09 -0300

    call new methods to add in mongodb

--- src/app/db/db_apostas.clj
+++ src/app/db/db_apostas.clj
@@ -1,4 +1,5 @@
-(ns app.db.db-apostas)
+(ns app.db.db-apostas
+  (:require [app.db.mongodb :refer [add-aposta-mongodb get-apostas-mongodb remove-aposta-mongodb]]))
 
 (def db-apostas (atom []))
 
@@ -9,14 +10,19 @@
         aposta (if (:id aposta)
                  aposta
                  (assoc (assoc aposta :status "ativo") :id (inc last-id)))]
-    (swap! db-apostas conj aposta)))
+    (do (swap! db-apostas conj aposta) (add-aposta-mongodb aposta))))
 
 (defn get-aposta [id]
   (first (filter #(= id (:id %)) @db-apostas)))
 
 (defn remove-aposta [id]
-  (swap! db-apostas #(remove (fn [aposta] (= id (:id aposta))) %)))
+  (swap! db-apostas #(remove (fn [aposta] (= id (:id aposta))) %))
+  (remove-aposta-mongodb id))
 
 (defn disable-aposta [id]
   (remove-aposta id)
   (swap! db-apostas conj (assoc (dissoc (get-aposta id) :status) :status "inativo")))
+
+(defn loading-apostas-mongodb []
+  (def apostas (map #(:apostas %) (get-apostas-mongodb)))
+  (reset! db-apostas apostas))
